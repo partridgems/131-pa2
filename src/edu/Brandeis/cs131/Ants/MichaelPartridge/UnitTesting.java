a9e2385e16485b6b0247d590f72e48f5faff4593
Michael Partridge <partridgems@gmail.com>
2014-10-24 16:16:41 -0400

    Useless
    Dead branch

--- src/edu/Brandeis/cs131/Ants/MichaelPartridge/UnitTesting.java
+++ src/edu/Brandeis/cs131/Ants/MichaelPartridge/UnitTesting.java
@@ -1,12 +1,22 @@
 package edu.Brandeis.cs131.Ants.MichaelPartridge;
 
+import edu.Brandeis.cs131.Ants.AbstractAnts.Animal;
+
 public class UnitTesting {
 
 	public static void main(String[] args) {
-
+		Animal animal = new Aardvark("Fred", null);
+		type(animal);
 		
 		
-		
+	}
+	
+	public static void type (Animal a) {
+		System.out.println("Animal");
+	}
+	
+	public static void type (Aardvark a) {
+		System.out.println("Aardvark");
 	}
 
 }
